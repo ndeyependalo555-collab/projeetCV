ab97fa4447be782a71a70c0c54ba3c1d69993935
ndeyependalo555-collab <ndeyependalo555@gmail.com>
2026-05-08 13:26:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,93 +1,95 @@
 import streamlit as st
 
-st.title("Welcome to my PORTFOLIO")
+st.title("Penda:Une diététicienne")
 #=======================
 #SIDEBAR
-# =======================
-st.sidebar.image("photo (2).jpeg",width=200)
+#=======================
 st.sidebar.header("Informations personnelles")
+st.sidebar.image("photo.jpeg",width=200)
 st.sidebar.markdown("""
-Nom : Penda LO  
-Âge : 20 ans  
-Statut : Célibataire  
-Niveau : Licence 2  
-Pays : Sénégal  
-Email : lo.penda@uam.edu.sn  
-Téléphone : +221 76 323 85 96  
+ Nom : Penda LO
+ Email : ndeyependalo555@gmail.com
+Si vous avez des questions, suggestions ou des idées de collaborations veuillez me contacter par l'adresse mail ci joint
 """)
-# ==================================== 
-# ====================================
+====================================
+====================================
 st.header("Profil")
 st.markdown("""
-Je suis étudiante en deuxième année en sciences et techniques agroalimentaires et nutritionnelles.  
-Ma formation m’a permis de développer des connaissances scientifiques solides ainsi qu’un esprit critique,  
-notamment dans la compréhension du lien entre alimentation et santé.
+Je suis étudiante en deuxième année en sciences et techniques agroalimentaires et nutritionnelles.
+ Ma formation m’a permis de développer des connaissances scientifiques solides ainsi qu’un esprit critique,
+ notamment dans la compréhension du lien entre alimentation et santé.
 Ce portfolio présente mon parcours académique, mes compétences et mes objectifs professionnels.
-- Étudiante motivée et sérieuse  
-- Intéressée par la nutrition et la santé publique  
-- Disciplinée et organisée  
-- Désireuse d’apprendre et de progresser  
-""")
-st.header("Parcours académique")
+• Étudiante motivée et sérieuse
+• Intéressée par la nutrition et la santé publique
+• Disciplinée et organisée
+• Désireuse d’apprendre, de progresser et d'aider à travers la diététique
+ """)
+
+st.header("Certifications")
 st.markdown("""
 Licence en Sciences et Techniques Agroalimentaires et Nutritionnelles (en cours)
-Polytech Diamniadio  
-- 2025-2026 : Licence 2  
-- 2024-2025 : Licence 1  
-- 2023-2024 : Baccalauréat S2 – Lycée Charles de Gaulle  
-Modules étudiés :
-- Nutrition  
-- Microbiologie alimentaire  
-- Biochimie  
-- Hygiène alimentaire  
-""")
+Polytech Diamniadio
+• 2024-2028: Licence 
+ Modules étudiés :
+- Nutrition
+- Biologie Appliquée aux sciences(Physique, Chimie, Statistique, Mathématiques, Informatique)         
+- Microbiologie alimentaire
+- Biotechnologie(Microbienne, Animale, Végétale)
+- Biochimie
+- Alimentation, Hygiène alimentaire
+- Transformation des produits locaux         
+• 2023-2024 : Baccalauréat S2          
+ """)
+
 st.header("Compétences")
 st.subheader("Compétences techniques")
 st.markdown("""
-- Analyse des informations nutritionnelles  
-- Compréhension de la composition des aliments  
-- Élaboration de conseils alimentaires  
-- Application des règles d’hygiène alimentaire  
+• Analyse des informations nutritionnelles (IMC, Glycémie,)
+• Compréhension de la composition des aliments
+• Élaboration de conseils alimentaires
+• Application des règles d’hygiène alimentaire
+ """)
+st.subheader("Compétences informatiques")
+st.markdorn("""
+• Canva
+• Suite bureatique
+• OTC
+• QGIS, Anaconda Navigator, R, VSCode
+• 
+""")            
+st.subheader("Soft skills")
+st.markdown("""         
+• Organisation
+• Esprit critique
+• Empathie
+• Sens de l'écoute
+• Capacité de motiver
 """)
-st.subheader("Compétences personnelles")
-st.markdown("""
-- Communication  
-- Travail en équipe  
-- Organisation  
-- Esprit critique  
-""")
-st.header("Distinction")
-st.markdown("""
-2022-2023 : Nominée parmi les dix meilleurs élèves de mon établissement
-""")
+
 st.header("Rôle du diététicien")
 st.markdown("""
 Le diététicien intervient dans la prévention et le traitement de plusieurs pathologies telles que :
-- Diabète  
-- Hypertension  
-- Obésité  
-- Malnutrition  
-Ses principales missions sont :
-- Élaboration de régimes alimentaires adaptés  
-- Conseil et suivi des patients  
-- Sensibilisation à une alimentation saine et équilibrée  
-""")
+• Diabète
+• Hypertension
+• Obésité
+• Malnutrition
+ Ses principales missions sont :
+• Élaboration de régimes alimentaires adaptés
+• Conseil et suivi des patients
+• Sensibilisation à une alimentation saine et équilibrée
+ """)
+
 st.header("Projet professionnel")
 st.markdown("""
-Dans un horizon de cinq ans, je souhaite devenir diététicienne qualifiée.  
-Mon objectif est d’exercer dans une structure hospitalière ou clinique, puis de développer :
-- Un centre de nutrition  
-- Une marque de produits alimentaires sains  
-Je souhaite également acquérir une reconnaissance à l’échelle internationale.
+Dans un horizon de cinq ans, je souhaite devenir diététicienne qualifiée.
+ Mon objectif est d’exercer dans une structure hospitalière ou clinique, puis de développer :
+• Un centre de nutrition
+• Une marque de produits alimentaires sains
+ Je souhaite également acquérir une reconnaissance à l’échelle internationale via un cabinet privé.
 """)
+
 st.header("Conclusion")
 st.markdown("""
-Je suis déterminée à contribuer à l’amélioration de la santé des populations à travers la nutrition.  
-Je suis actuellement à la rechserche d’opportunités me permettant de développer mes compétences professionnelles.
-""")
-st.markdown("""
----
-Si vous avez des questions, suggestions ou des idées de collaborations veuillez me contacter:
-Email : lo.penda@uam.edu.sn  
-Téléphone : +221 76 323 85 96  
+Je suis déterminée à contribuer à l’amélioration de la santé des populations à travers la nutrition.
+ Je suis actuellement à la recherche d’opportunités me permettant de développer mes compétences professionnelles.
 """)
