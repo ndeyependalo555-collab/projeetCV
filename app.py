89e98b30a6cbd1970ab9d55125675e7cd92376e5
ndeyependalo555-collab <ndeyependalo555@gmail.com>
2026-05-08 12:45:00 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+
+st.title("Welcome to my PORTFOLIO")
+#=======================
+#SIDEBAR
+# =======================
+st.sidebar.image("photo (2).jpeg",width=200)
+st.sidebar.header("Informations personnelles")
+st.sidebar.markdown("""
+Nom : Penda LO  
+Âge : 20 ans  
+Statut : Célibataire  
+Niveau : Licence 2  
+Pays : Sénégal  
+Email : lo.penda@uam.edu.sn  
+Téléphone : +221 76 323 85 96  
+""")
+# ==================================== 
+# ====================================
+st.header("Profil")
+st.markdown("""
+Je suis étudiante en deuxième année en sciences et techniques agroalimentaires et nutritionnelles.  
+Ma formation m’a permis de développer des connaissances scientifiques solides ainsi qu’un esprit critique,  
+notamment dans la compréhension du lien entre alimentation et santé.
+Ce portfolio présente mon parcours académique, mes compétences et mes objectifs professionnels.
+- Étudiante motivée et sérieuse  
+- Intéressée par la nutrition et la santé publique  
+- Disciplinée et organisée  
+- Désireuse d’apprendre et de progresser  
+""")
+st.header("Parcours académique")
+st.markdown("""
+Licence en Sciences et Techniques Agroalimentaires et Nutritionnelles (en cours)
+Polytech Diamniadio  
+- 2025-2026 : Licence 2  
+- 2024-2025 : Licence 1  
+- 2023-2024 : Baccalauréat S2 – Lycée Charles de Gaulle  
+Modules étudiés :
+- Nutrition  
+- Microbiologie alimentaire  
+- Biochimie  
+- Hygiène alimentaire  
+""")
+st.header("Compétences")
+st.subheader("Compétences techniques")
+st.markdown("""
+- Analyse des informations nutritionnelles  
+- Compréhension de la composition des aliments  
+- Élaboration de conseils alimentaires  
+- Application des règles d’hygiène alimentaire  
+""")
+st.subheader("Compétences personnelles")
+st.markdown("""
+- Communication  
+- Travail en équipe  
+- Organisation  
+- Esprit critique  
+""")
+st.header("Distinction")
+st.markdown("""
+2022-2023 : Nominée parmi les dix meilleurs élèves de mon établissement
+""")
+st.header("Rôle du diététicien")
+st.markdown("""
+Le diététicien intervient dans la prévention et le traitement de plusieurs pathologies telles que :
+- Diabète  
+- Hypertension  
+- Obésité  
+- Malnutrition  
+Ses principales missions sont :
+- Élaboration de régimes alimentaires adaptés  
+- Conseil et suivi des patients  
+- Sensibilisation à une alimentation saine et équilibrée  
+""")
+st.header("Projet professionnel")
+st.markdown("""
+Dans un horizon de cinq ans, je souhaite devenir diététicienne qualifiée.  
+Mon objectif est d’exercer dans une structure hospitalière ou clinique, puis de développer :
+- Un centre de nutrition  
+- Une marque de produits alimentaires sains  
+Je souhaite également acquérir une reconnaissance à l’échelle internationale.
+""")
+st.header("Conclusion")
+st.markdown("""
+Je suis déterminée à contribuer à l’amélioration de la santé des populations à travers la nutrition.  
+Je suis actuellement à la rechserche d’opportunités me permettant de développer mes compétences professionnelles.
+""")
+st.markdown("""
+---
+Si vous avez des questions, suggestions ou des idées de collaborations veuillez me contacter:
+Email : lo.penda@uam.edu.sn  
+Téléphone : +221 76 323 85 96  
+""")
